b00bef2cc65c36151214c336c4e89f8d2a49174a
kurup87 <ammaranuar87@gmail.com>
2017-08-21 06:26:23 +0800

    First commit

---
+++ app/Http/routes.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    });
+
+    /**
+     * Upload Task
+     */
+    function update(Request $request, Task $task)
+    {
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255',
+        ]);
+
+        $task = Task::findOrFail($task->id);
+
+        $request->user()->tasks()->update([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+
+        return redirect('/');
+    }
+
+});
